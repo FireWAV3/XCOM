6a6127de662ce1905cc6851e9e952bac68413ad2
Jaserro <63459397+Jaserro@users.noreply.github.com>
2020-11-03 13:24:55 +0100

    Revert "Update Field.scala"
    This reverts commit 2f3dd97caf172bc428f64acb4f3692f732eb6394.

--- src/main/scala/XCOM/Field.scala
+++ src/main/scala/XCOM/Field.scala
@@ -8,10 +8,39 @@ case class Field(pX: Int, pY: Int, cells: Vector[Cell], character: Vector[Charac
   override def toString: String = {
     var vectorcountF, vectorcountC = 0
     var temp = "\t "
+    /*var abc = "A" //ersetzt durch xAxisString
+    for (i <- 0 to sizeX) {
+      temp += (abc + "\t")
+      abc = (abc(0) + 1).toChar.toString
+    }
+    temp += "\n  "
+    for (i <- 0 to sizeX) {
+      temp += "----"
+    }
+    temp += "\n"*/
     temp += xAxisString(sizeX)
     for (i <- 0 to sizeY) {
       temp += (i + 1 + "\t|")
-      temp += printRow(sizeX, i, this.cells, character)
+      for (j <- 0 to sizeX) {
+
+        /*if (character.length > vectorcountC &&
+          character(vectorcountC).cell.x == j &&
+          character(vectorcountC).cell.y == i) {
+          temp += this.character(vectorcountC).displayname
+          temp += "\t"
+          vectorcountC += 1
+
+        } else if (cells.length > vectorcountF &&
+          cells(vectorcountF).x == j &&
+          cells(vectorcountF).y == i) {
+          temp += this.cells(vectorcountF).otype
+          temp += "\t"
+          vectorcountF += 1
+        } else {
+          temp += "X\t"
+        }*/
+        temp += fieldPosReturn(j,i,this.cells,character)
+      }
       temp += "\n"
     }
 
@@ -35,24 +64,16 @@ case class Field(pX: Int, pY: Int, cells: Vector[Cell], character: Vector[Charac
   def fieldPosReturn(x:Int, y:Int, rocks:Vector[Cell], player:Vector[Character]) :String = {
     player.foreach{ p =>
       if(p.cell.x == x && p.cell.y == y){
-        return Console.BLUE + p.displayname + "\t" + Console.RESET
+        return p.displayname + "\t"
       }
     }
 
     rocks.foreach{ r =>
       if(r.x == x && r.y == y){
-        return Console.WHITE_B + Console.BLACK + r.otype + Console.RESET + "\t"//If otype is Enum, use toString of otype
+        return r.otype + "\t" //Falls otype Enum, hier toString von otype
       }
     }
     return "X\t"
-  }
-
-  def printRow (xLength:Int, yRow:Int, rocks:Vector[Cell], player:Vector[Character]) :String = {
-    var retString = ""
-    for (j <- 0 to xLength){
-      retString += fieldPosReturn(j, yRow, rocks, player)
-    }
-    retString
   }
 }
 
