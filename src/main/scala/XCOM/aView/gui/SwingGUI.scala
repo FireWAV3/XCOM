823dff4e3ed9f57677049ceb8428f6f738d68900
Jaserro <63459397+Jaserro@users.noreply.github.com>
2020-12-15 17:43:51 +0100

    init of menu in gui

--- src/main/scala/XCOM/aView/gui/SwingGUI.scala
+++ src/main/scala/XCOM/aView/gui/SwingGUI.scala
@@ -1,5 +1,6 @@
 package XCOM.aView.gui
 import XCOM.controller.{Controller, UpdateField}
+import javax.swing.ImageIcon
 
 import scala.swing._
 import scala.swing.event.MouseClicked
@@ -7,6 +8,29 @@ import scala.swing.event.MouseClicked
 class SwingGUI(c: Controller) extends Frame {
   listenTo(c)
   title = "Xcom"
+  var menu = new BoxPanel(Orientation.Vertical){
+    contents += new Label(){
+      icon = new ImageIcon("/img/xcom_menu_icon.png")
+    }
+    contents += new Label(){
+      text = "Welcome to XCOM!"
+    }
+    contents += new BoxPanel(Orientation.Horizontal){
+      contents += new Label(){
+        text = "Please enter a Number between 1 and " + c.scenarioAmmount
+      }
+      contents += new TextField(){
+      }
+    }
+    contents += new Label(){
+      text = "Go!"
+      listenTo(mouse.clicks)
+      reactions +={
+        case MouseClicked(scr,pt,mod,clicks,pops) => c.loadScenario(1)
+      }
+    }
+  }
+
   val label = new Label(){
     text = "Welcome to XCOM!"
   }
@@ -18,7 +42,9 @@ class SwingGUI(c: Controller) extends Frame {
     }
   }
   contents = bp
+  contents = menu
   maximize()
+  resizable = false
   open()
   reactions += {
     case event: UpdateField => label.text = c.output; repaint()
